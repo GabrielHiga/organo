36b32c87102d3a5e74dfb95c3f58d68f45da1482
Your Name <gabriel_hideki@live.com>
2022-10-27 21:46:15 -0300

    Adding a Delete button

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,16 +1,18 @@
 import Colaborador from '../Colaborador'
 import './time.css'
 
-const Time = ({ time, colaboradores }) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
-
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+        colaboradores.length > 0 && 
+        <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+            <input onChange={evento => mudarCor(evento.target.value, time.nome)} type='color' value={time.corSecundaria} className='input-color' />
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} />)}
+                {colaboradores.map((colaborador, indice) => {
+                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar}/>
+                })}
             </div>
         </section>
-
     )
 }
 
